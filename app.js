34941c510ebfb58564567241df901ce4693133d6
danafan <13067882143@163.com>
2023-08-07 15:26:35 +0800

    打包页面新增列表数量

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 App({
   globalData: {
-    // baseurl: "http://erpdeer.92nu.com/api/",          //德儿正式/
+    baseurl: "http://erpdeer.92nu.com/api/",          //德儿正式/
     // baseurl: "http://erphuda.92nu.com/api/",          //乎达正式
     // baseurl: "http://erpwp.92nu.com//api/",           //唯品正式
-    baseurl: "http://erpcs.92nu.com/api/",               //测试
+    // baseurl: "http://erpcs.92nu.com/api/",               //测试
     username: "",                                        //用户名
     is_all_package:'0',                                  //是否显示全部记录(0:不显示；1:显示)
     printer:"",                                          //选择的打印机
