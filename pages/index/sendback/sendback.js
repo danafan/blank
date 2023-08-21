77d0ba21f968ab93ac95389b7f2e4cfea71179fc
danafan <13067882143@163.com>
2023-08-21 14:26:29 +0800

    商品退回新增仓库选择

--- pages/index/sendback/sendback.js
+++ pages/index/sendback/sendback.js
@@ -2,6 +2,8 @@ Page({
   data: {
     sku: "",            //唯一码
     type:0,            //是否是唯一码
+    objectArray: [],    //仓库列表
+    arrIndex: 0,        //默认选中的仓库下标
     num:1,
     cause: "",          //退货原因
     title: "",           //顶部标题
@@ -12,11 +14,40 @@ Page({
     })
     //验证商品唯一码
     this.verify();
+    //获取仓库列表
+    this.getStash();
   },
   bindKeyInput(e){
     this.setData({
       num:e.detail.value
     })
+  },
+  //获取仓库列表
+  getStash(){
+    dd.httpRequest({
+      url: getApp().globalData.baseurl + 'goods/return_wms',
+      method: 'GET',
+      data: {},
+      dataType: 'json',
+      success: (res) => {
+        var data = res.data;
+        if (data.code == 1) {
+          let default_index = data.data.findIndex(item => {
+            return item.is_default == 1;
+          })
+          this.setData({
+            objectArray:data.data,
+            arrIndex:default_index
+          })
+        } else {
+          dd.showToast({
+            type: 'none',
+            content: data.msg,
+            duration: 2000
+          });
+        }
+      }
+    });
   },
   //验证商品唯一码
   verify() {
@@ -55,6 +86,12 @@ Page({
       cause: e.detail.value
     });
   },
+  //切换仓库
+  bindObjPickerChange(e) {
+    this.setData({
+      arrIndex: e.detail.value,
+    });
+  },
   //点击提交
   submit() {
     if(this.data.num == ''){
@@ -85,7 +122,8 @@ Page({
         data: {
           unique_no: this.data.sku,
           reason: this.data.cause,
-          num:this.data.num
+          num:this.data.num,
+          wms_co_id:this.data.objectArray[this.data.arrIndex].wms_co_id
         },
         dataType: 'json',
         success: (res) => {
