e12221f98a562ee46dc9ef6d345b6e4bb7e70159
danafan <13067882143@163.com>
2023-11-15 15:53:54 +0800

    商品退回新增供应商选择

--- pages/index/sendback/sendback.js
+++ pages/index/sendback/sendback.js
@@ -1,29 +1,31 @@
 Page({
   data: {
-    sku: "",            //唯一码
-    type:0,            //是否是唯一码
-    objectArray: [],    //仓库列表
-    arrIndex: 0,        //默认选中的仓库下标
-    num:1,
-    cause: "",          //退货原因
-    title: "",           //顶部标题
+    sku: "", //唯一码
+    type: 0, //是否是唯一码
+    objectArray: [], //仓库列表
+    arrIndex: 0, //默认选中的仓库下标
+    supplierArray: [], //供应商列表
+    supplierIndex: -1, //选中的供应商下标
+    num: 1,
+    cause: "", //退货原因
+    title: "", //顶部标题
   },
   onLoad(e) {
     this.setData({
-      sku:getApp().globalData.codeObj
+      sku: getApp().globalData.codeObj
     })
     //验证商品唯一码
     this.verify();
     //获取仓库列表
     this.getStash();
   },
-  bindKeyInput(e){
+  bindKeyInput(e) {
     this.setData({
-      num:e.detail.value
+      num: e.detail.value
     })
   },
   //获取仓库列表
-  getStash(){
+  getStash() {
     dd.httpRequest({
       url: getApp().globalData.baseurl + 'goods/return_wms',
       method: 'GET',
@@ -36,8 +38,8 @@ Page({
             return item.is_default == 1;
           })
           this.setData({
-            objectArray:data.data,
-            arrIndex:default_index
+            objectArray: data.data,
+            arrIndex: default_index
           })
         } else {
           dd.showToast({
@@ -61,9 +63,20 @@ Page({
       success: (res) => {
         var data = res.data;
         if (data.code == 1) {
+          let supplier_list = data.data.supplier_list;
+          let obj = {
+            supplier_id: data.data.supplier_id,
+            supplier_name: data.data.supplier_name
+          }
+          supplier_list.unshift(obj);
+          let supplier_index = supplier_list.findIndex(item => {
+            return item.supplier_id == data.data.supplier_id;
+          })
           this.setData({
             sku: getApp().globalData.codeObj,
-            type:data.data.type
+            supplierArray: supplier_list,
+            supplierIndex: supplier_index,
+            type: data.data.type
           })
         } else {
           dd.showToast({
@@ -92,21 +105,27 @@ Page({
       arrIndex: e.detail.value,
     });
   },
+  //切换供应商
+  changeSupplier(e) {
+    this.setData({
+      supplierIndex: e.detail.value,
+    });
+  },
   //点击提交
   submit() {
-    if(this.data.num == ''){
+    if (this.data.num == '') {
       dd.showToast({
         type: 'none',
         content: '数量不能为空',
         duration: 2000
       });
-    }else if (this.data.num <= 0) {
+    } else if (this.data.num <= 0) {
       dd.showToast({
         type: 'none',
         content: '数量必须大于0',
         duration: 2000
       });
-    }else if (this.data.cause == "") {
+    } else if (this.data.cause == "") {
       dd.showToast({
         type: 'none',
         content: '请输入退错原因',
@@ -122,8 +141,9 @@ Page({
         data: {
           unique_no: this.data.sku,
           reason: this.data.cause,
-          num:this.data.num,
-          wms_co_id:this.data.objectArray[this.data.arrIndex].wms_co_id
+          num: this.data.num,
+          wms_co_id: this.data.objectArray[this.data.arrIndex].wms_co_id,
+          supplier_id: this.data.supplierArray[this.data.supplierIndex].supplier_id,
         },
         dataType: 'json',
         success: (res) => {
